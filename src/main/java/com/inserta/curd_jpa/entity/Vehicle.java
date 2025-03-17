12f33dc1c42adac5a7c19aec23908440442287b7
HenryRisco <lhrisco@gmail.com>
2025-03-17 13:03:34 +0100

    Implement abstract class Vehicle

---
+++ src/main/java/com/inserta/curd_jpa/entity/Vehicle.java
@@ -0,0 +1,45 @@
+package com.inserta.curd_jpa.entity;
+
+public class Vehicle {
+    private String brand;
+    private String model;
+    private String carRegistration;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String carRegistration, String model, String brand) {
+        this.carRegistration = carRegistration;
+        this.model = model;
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getCarRegistration() {
+        return carRegistration;
+    }
+
+    public void setCarRegistration(String carRegistration) {
+        this.carRegistration = carRegistration;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", carRegistration='" + carRegistration + '\'' +
+                '}';
+    }
+}
