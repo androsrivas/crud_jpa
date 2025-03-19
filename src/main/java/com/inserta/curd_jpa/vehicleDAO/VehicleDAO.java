7c2753d3b11616ba24ff88502523f4e032739e52
lltosso <luis.tosso@gmail.com>
2025-03-19 13:11:40 +0100

    add Id and delete in DAO

--- src/main/java/com/inserta/curd_jpa/vehicleDAO/VehicleDAO.java
+++ src/main/java/com/inserta/curd_jpa/vehicleDAO/VehicleDAO.java
@@ -1,4 +1,13 @@
 package com.inserta.curd_jpa.vehicleDAO;
 
+import com.inserta.curd_jpa.entity.Vehicle;
+
 public interface VehicleDAO {
+
+    void save(Vehicle vehicle);
+
+    Vehicle findById(Integer id);
+
+    void delete(Vehicle vehicle);
+    Vehicle delete()
 }
--- src/main/java/com/inserta/curd_jpa/vehicleDAO/VehicleDAOImpl.java
+++ src/main/java/com/inserta/curd_jpa/vehicleDAO/VehicleDAOImpl.java
@@ -1,4 +1,26 @@
 package com.inserta.curd_jpa.vehicleDAO;
 
-public class VehicleDAOImpl {
+import com.inserta.curd_jpa.entity.Vehicle;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class VehicleDAOImpl implements VehicleDAO{
+    private EntityManager entityManager;
+
+    @Autowired
+    public VehicleDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    @Override
+    @Transactional
+    public void save(Vehicle vehicle){
+        this.entityManager.persist(vehicle);
+    }
+
+    @Override
+    public Vehicle findById(Integer id) {
+        return null;
+    }
+
 }
